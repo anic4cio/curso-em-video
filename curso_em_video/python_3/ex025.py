32ae862106950ff4e491de9d3652d4bf699e0f1c
Vinicius Anicácio <anicaciovinicius36@gmail.com>
2022-02-09 14:13:31 -0300

    Procurando uma sting dentro de outra

---
+++ curso_em_video/python_3/ex025.py
@@ -0,0 +1,6 @@
+"""
+Exercício Python 25: Crie um programa que leia o nome de uma pessoa e diga se ela tem “SILVA” no nome.
+"""
+
+nome = str(input('Insira seu nome completo: ')).strip().title().split()
+print('Silva' in nome)
