b7b1a599d4115f4aa7cc76331cde7c65124eb2fe
Vinicius Anicácio <anicaciovinicius36@gmail.com>
2022-02-09 16:24:57 -0300

    Lista composta e análise de dados

---
+++ curso_em_video/python_3/ex084.py
@@ -0,0 +1,39 @@
+"""
+Exercício Python 084: Faça um programa que leia nome e peso de várias pessoas, guardando tudo em uma lista.
+No final, mostre:
+A) Quantas pessoas foram cadastradas.
+B) Uma listagem com as pessoas mais pesadas.
+C) Uma listagem com as pessoas mais leves.
+"""
+
+lista = []
+listinha = []
+continuar = 'S'
+pessoas = 0
+while continuar == 'S':
+    nome = input('Nome: ')
+    peso = float(input('Peso: '))
+    listinha.append(nome)
+    listinha.append(peso)
+    lista.append(listinha[:])
+    listinha.clear()
+    if pessoas == 0:
+        pesado = peso
+        leve = peso
+    elif peso > pesado:
+        pesado = peso
+    elif peso < leve:
+        leve = peso
+    pessoas += 1
+    continuar = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
+print(f'Você cadastrou {pessoas} pessoas.')
+print(f'O maior peso foi {pesado} Kg. De ', end='')
+for x in lista:
+    if x[1] == pesado:
+        print(x[0], end=' ')
+print(f'\nO menor peso foi {leve} Kg. De ', end='')
+for t in lista:
+    if t[1] == leve:
+        print(t[0], end=' ')
+
+
