42df8bc6a2ffbe54209607e7eb0a97ff7c04791f
Vinicius Anicácio <anicaciovinicius36@gmail.com>
2022-02-09 14:04:16 -0300

    Quebrando um Número

---
+++ curso_em_video/python_3/ex016.py
@@ -0,0 +1,8 @@
+"""
+Exercício Python 16:
+Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
+"""
+
+print('Digite um número fracionado e o programa o transformará em inteiro.')
+x = float(input('Digire um número fracionado: '))
+print(int(x))
