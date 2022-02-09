7dddd22a593072811fabc0a953d5ef30f52527c0
Vinicius Anicácio <anicaciovinicius36@gmail.com>
2022-02-09 15:58:38 -0300

    Contagem de pares

---
+++ curso_em_video/python_3/ex047.py
@@ -0,0 +1,8 @@
+"""
+Exercício Python 47: Crie um programa que mostre na tela todos os números pares
+ que estão no intervalo entre 1 e 50.
+"""
+
+print('Números pares de 0 a 50')
+for pares in range(0, 51, 2):
+    print(pares, end=' ')
