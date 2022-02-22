c920eee3ebb98e73c1532440e64f089b89ecae48
anic4cio <anicaciovinicius36@gmail.com>
2022-02-22 20:41:00 -0300

    Exercitando módulos em Python

--- curso_em_video/python_3/ex107.py
+++ curso_em_video/python_3/ex107.py
@@ -4,3 +4,10 @@ aumentar(), diminuir(), dobro() e metade(). Faça também um programa que import
 módulo e use algumas dessas funções.
 """
 
+import moeda 
+valor = int(input('Digite um número e veja: '))
+print(f'Dobro de {valor}: {moeda.dobro(valor)}')
+print(f'Triplo de {valor}: {moeda.triplo(valor)}')
+print(f'{valor} menos 15%: {moeda.quinze_menos(valor)}')
+print(f'{valor} mais 15%: {moeda.quinze_mais(valor)}')
+
