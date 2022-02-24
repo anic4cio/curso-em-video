cbf23dab271a78f15d64f3c94baaee86f0ffffba
anic4cio <anicaciovinicius36@gmail.com>
2022-02-24 12:19:19 -0300

    Exercitando Módulos em Python

---
+++ curso_em_video/python_3/ex107/ex107.py
@@ -0,0 +1,13 @@
+"""
+Exercício Python 107: Crie um módulo chamado moeda.py que tenha as funções incorporadas 
+aumentar(), diminuir(), dobro() e metade(). Faça também um programa que importe esse 
+módulo e use algumas dessas funções.
+"""
+
+import moeda 
+valor = int(input('Digite um número e veja: '))
+print(f'Dobro de {valor}: {moeda.dobro(valor)}')
+print(f'Triplo de {valor}: {moeda.triplo(valor)}')
+print(f'{valor} menos 10%: {moeda.porcentagem_menos(valor, 10)}')
+print(f'{valor} mais 10%: {moeda.porcentagem_mais(valor, 10)}')
+
---
+++ curso_em_video/python_3/ex107/moeda.py
@@ -0,0 +1,14 @@
+def porcentagem_mais(numero, taxa):
+    total = numero + (numero * taxa) / 100
+    return total
+
+
+def porcentagem_menos(numero, taxa):
+    total = numero - (numero * taxa) / 100
+    return total
+
+def dobro(numero):
+    return numero * 2
+
+def triplo(numero):
+    return numero * 3
