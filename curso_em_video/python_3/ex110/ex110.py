4e4e20ce999234b28b957792d21bd77850982df9
anic4cio <anicaciovinicius36@gmail.com>
2022-02-24 15:33:05 -0300

    Reduzindo ainda mais o seu código

---
+++ curso_em_video/python_3/ex110/ex110.py
@@ -0,0 +1,9 @@
+"""
+Exercício Python 110: Adicione o módulo moeda.py criado nos desafios anteriores, uma função chamada resumo(), 
+que mostre na tela algumas informações geradas pelas funções que já temos no módulo criado até aqui.
+"""
+
+import moedav4
+
+valor = int(input('Digite um valor: R$ '))
+moedav4.resumo(valor, 50, 10)
